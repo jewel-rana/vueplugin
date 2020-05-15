430b94b908af6c3f531ae7137876f9e9babec86e
Jewel Rana <jewelrana.dev@gmail.com>
2020-05-15 07:17:29 +0600

    Working on vue plugin

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import CommentsOverlay from './plugins/overlaycomments'
 
+Vue.use(CommentsOverlay)
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/plugins/overlaycomments/index.js
@@ -0,0 +1,8 @@
+// src/plugins/CommentsOverlay/index.js
+// 
+export default {
+    install(vue, opts){   
+        console.log('Installing the CommentsOverlay plugin!')
+        // Fun will happen here
+    }
+}
